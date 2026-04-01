0832ef4d3ad183b2af3f1b19788a0dc5762f0686
G'lek Tarssza <glek@glektarssza.com>
2026-04-01 08:38:16 -0600

    Fixed a few mis-labelled Javadocs.

--- src/main/java/com/glektarssza/gtnh_customizer/utils/PotionUtils.java
+++ src/main/java/com/glektarssza/gtnh_customizer/utils/PotionUtils.java
@@ -281,7 +281,7 @@ public class PotionUtils {
     /**
      * Try to check if a potion is a negative potion by its potion effect.
      *
-     * @param id The potion effect to check.
+     * @param potionEffect The potion effect to check.
      *
      * @return {@code true} if the potion is a negative potion, {@code false} if
      *         it is not, an empty {@link Optional} otherwise.
@@ -350,7 +350,7 @@ public class PotionUtils {
     /**
      * Try to check if a potion is a positive potion by its potion effect.
      *
-     * @param id The potion effect to check.
+     * @param potionEffect The potion effect to check.
      *
      * @return {@code true} if the potion is a positive potion, {@code false} if
      *         it is not, an empty {@link Optional} otherwise.
